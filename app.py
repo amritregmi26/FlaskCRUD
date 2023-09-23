ff5e846e19aa4b948bc0c34291855fb9aedc324c
= <=>
2023-09-23 17:37:12 +0545

    flask setup completed

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def create_user():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True, port=5001)
